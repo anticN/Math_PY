01148c1c5bff70289614c3a3bc894bd0f37e8c69
Sami Hindi <sami@samihindi.com>
2022-10-05 08:03:36 +0200

    Removed Setters from quadraticFunction and removed input taking in constructor.

--- quadraticFunction.py
+++ quadraticFunction.py
@@ -3,33 +3,28 @@ class QuadraticFunction:
     This class describes a quadratic function with the attributes a(slope), b and c (y axial intercept)
     """
     def __init__(self, a, b, c):
-        self.__a = float(input("Enter value for a(slope): "))
-        self.__b = float(input("Enter value for b: "))
-        self.__c = float(input("Enter value for b(y axial intercept), if none just type 0: "))
+        #self.__a = float(input("Enter value for a(slope): "))
+        #self.__b = float(input("Enter value for b: "))
+        #self.__c = float(input("Enter value for b(y axial intercept), if none just type 0: "))
+        self.__a = a
+        self.__b = b
+        self.__c = c
 
     @property
     def a(self):
+        """
+        Getter function for a value.
+        :return: a
+        """
         return self.__a
-
-    @a.setter
-    def a(self, a):
-        self.__a = a
 
     @property
     def b(self):
         return self.__b
 
-    @b.setter
-    def b(self, b):
-        self.__b = b
-
     @property
     def c(self):
         return self.__c
-
-    @c.setter
-    def c(self, c):
-        self.__c = c
 
     def print(self):
         pass
